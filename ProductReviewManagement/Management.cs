e9b426fabdcc56cd66bde0b847a9fc8b6fca86bf
mmkr7111 <mmkr7111@gmail.com>
2020-11-02 19:59:33 +0530

    [Manish Reddy]Add: Implemented Linq problem statement Use cases

---
+++ ProductReviewManagement/Management.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Data;
+
+namespace ProductReviewManagement
+{
+    public class Management
+    {
+        public readonly DataTable dataTable = new DataTable();
+        /// <summary>
+        /// UC-2
+        /// </summary>
+        /// <param name="review"></param>
+        public void TopRecords(List<ProductReview> review)
+        {
+
+            var recordedData = (from productReviews in review
+                               orderby productReviews.Rating descending
+                               select productReviews).Take(3);
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID
+                    + " " + "Rating:-" + list.Rating + " " + "Review:-" + list.Review + " " + "isLike:-" + list.isLike);
+            }
+        }
+        /// <summary>
+        /// UC-3
+        /// </summary>
+        /// <param name="review"></param>
+        public void SelectedRecords(List<ProductReview> review)
+        {
+            var recordedData = from productReviews in review
+                               where (productReviews.ProductID==1 && productReviews.Rating>3)||(productReviews.ProductID==4 && productReviews.Rating > 3 )||
+                               (productReviews.ProductID==9 && productReviews.Rating > 3)
+                               select productReviews;
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID
+                    + " " + "Rating:-" + list.Rating + " " + "Review:-" + list.Review + " " + "isLike:-" + list.isLike);
+            }
+        }
+        /// <summary>
+        /// UC-4
+        /// </summary>
+        /// <param name="review"></param>
+        public void RetrieveCountOfRecords(List<ProductReview> review)
+        {
+            var recordedData = review.GroupBy(x => x.ProductID).Select(x => new { ProductID = x.Key, Count = x.Count() });
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine(list.ProductID+"-----"+list.Count);
+            }
+        }
+        /// <summary>
+        /// UC-5
+        /// </summary>
+        /// <param name="review"></param>
+        public void RetrieveProductIdAndReview(List<ProductReview> review)
+        {
+            var recordedData = from productReviews in review
+                               select new { productReviews.ProductID, productReviews.Review };
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:-" + list.ProductID + " " + "Review:-" + list.Review );
+            }
+        }
+        /// <summary>
+        /// UC-6
+        /// </summary>
+        /// <param name="review"></param>
+        public void SkipTopRecords(List<ProductReview> review)
+        {
+            var recordedData = (from productReviews in review
+                               select productReviews).Skip(5);
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID
+                    + " " + "Rating:-" + list.Rating + " " + "Review:-" + list.Review + " " + "isLike:-" + list.isLike);
+            }
+        }
+        /// <summary>
+        /// UC-8
+        /// </summary>
+        /// <param name="review"></param>
+        public void ProductReviewsDataTable(List<ProductReview> review)
+        {
+            var recordedData = from productReviews in review
+                                select productReviews;
+            dataTable.Columns.Add("ProductId").DataType = typeof(Int32);
+            dataTable.Columns.Add("UserId").DataType = typeof(Int32);
+            dataTable.Columns.Add("Rating").DataType = typeof(double);
+            dataTable.Columns.Add("Review");
+            dataTable.Columns.Add("isLike").DataType = typeof(bool);
+            foreach (var list in recordedData)
+            {
+                dataTable.Rows.Add(list.ProductID, list.UserID, list.Rating, list.Review, list.isLike);
+            }
+            var productTable = from products in dataTable.AsEnumerable() select products;
+            foreach (DataRow product in productTable)
+            {
+                Console.WriteLine(product.Field<int>("productId") + " " + product.Field<int>("UserId") + " "
+                    + product.Field<double>("Rating") + " " + product.Field<string>("Review") + " " + product.Field<bool>("isLike"));
+            }
+        }
+        /// <summary>
+        /// UC-9
+        /// </summary>
+        /// <param name="review"></param>
+        public void RetriveRecordsFromDataTable()
+        {
+            var productTable = from products in this.dataTable.AsEnumerable() where products.Field<bool>("isLike").Equals(true) select products;
+            foreach (DataRow product in productTable)
+            {
+                Console.WriteLine(product.Field<int>("productId") + " " + product.Field<int>("UserId") + " "
+                    + product.Field<double>("Rating") + " " + product.Field<string>("Review") + " " + product.Field<bool>("isLike"));
+            }
+        }
+        /// <summary>
+        /// UC-10
+        /// </summary>
+        /// <param name="review"></param>
+        public void AveragePerProductId(List<ProductReview> review)
+        {
+            var recordedData = review.GroupBy(x => x.ProductID).Select(x => new { ProductID = x.Key, AverageRating = x.Average(x=>x.Rating) });
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine(list.ProductID + "-----" + list.AverageRating);
+            }
+        }
+        /// <summary>
+        /// UC-11
+        /// </summary>
+        /// <param name="review"></param>
+        public void RetrieveRecordsUsingReview(List<ProductReview> review,string reviewPoint)
+        {
+            var recordedData = from productReviews in review
+                               where productReviews.Review==reviewPoint
+                               select productReviews;
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID
+                    + " " + "Rating:-" + list.Rating + " " + "Review:-" + list.Review + " " + "isLike:-" + list.isLike);
+            }
+        }
+        /// <summary>
+        /// UC-12
+        /// </summary>
+        /// <param name="review"></param>
+        public void RetriveRecordsFromDataTableUsingUserID()
+        {
+            var productTable = from products in this.dataTable.AsEnumerable() where products.Field<int>("UserId")==10 select products;
+            foreach (DataRow product in productTable)
+            {
+                Console.WriteLine(product.Field<int>("productId") + " " + product.Field<int>("UserId") + " "
+                    + product.Field<double>("Rating") + " " + product.Field<string>("Review") + " " + product.Field<bool>("isLike"));
+            }
+        }
+    }
+}
---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product review management problem statement");
+
+            //UC-1
+            List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ProductID=1,UserID=1,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ProductID=2,UserID=1,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProductID=3,UserID=2,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ProductID=4,UserID=2,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProductID=5,UserID=3,Rating=2,Review="nice",isLike=false},
+                new ProductReview(){ProductID=6,UserID=4,Rating=1,Review="Bad",isLike=false},
+                new ProductReview(){ProductID=1,UserID=3,Rating=1.5,Review="nice",isLike=false},
+                new ProductReview(){ProductID=11,UserID=10,Rating=4,Review="nice",isLike=true},
+                new ProductReview(){ProductID=10,UserID=10,Rating=4,Review="nice",isLike=true},
+                new ProductReview(){ProductID=12,UserID=10,Rating=4,Review="nice",isLike=true},
+                new ProductReview(){ProductID=13,UserID=10,Rating=4,Review="nice",isLike=true},
+                new ProductReview(){ProductID=14,UserID=10,Rating=4,Review="nice",isLike=true},
+                new ProductReview(){ProductID=15,UserID=10,Rating=4,Review="nice",isLike=true},
+                new ProductReview(){ProductID=16,UserID=10,Rating=4,Review="nice",isLike=true}
+            };
+            //foreach(var list in productReviewList)
+            //{
+            //    Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID 
+            //        + " " + "Rating:-" + list.Rating + " " + "Review:-" + list.Review + " " + "isLike:-" + list.isLike);
+            //}
+
+            //UC-2
+            Management management = new Management();
+            //management.TopRecords(productReviewList);
+
+            //UC-3
+            // management.SelectedRecords(productReviewList);
+            //UC-4
+            //management.RetrieveCountOfRecords(productReviewList);
+            //UC-5
+            //management.RetrieveProductIdAndReview(productReviewList);
+            //UC-6
+            //management.SkipTopRecords(productReviewList);
+            //UC-7
+            List<ProductReview> ReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ProductID=10,UserID=1,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ProductID=20,UserID=1,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProductID=30,UserID=2,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ProductID=40,UserID=2,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProductID=50,UserID=3,Rating=2,Review="Not bad",isLike=false},
+                new ProductReview(){ProductID=60,UserID=4,Rating=1,Review="Bad",isLike=false},
+                new ProductReview(){ProductID=70,UserID=3,Rating=1.5,Review="Bad",isLike=false},
+                new ProductReview(){ProductID=90,UserID=3,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProductID=90,UserID=3,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProductID=90,UserID=3,Rating=3.5,Review="nice",isLike=true}
+            };
+            var finalReviewList = productReviewList.Concat(ReviewList);
+            //foreach (var list in finalReviewList)
+            //{
+            //    Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID
+            //        + " " + "Rating:-" + list.Rating + " " + "Review:-" + list.Review + " " + "isLike:-" + list.isLike);
+            //}
+            //UC-8,UC-9,UC-12
+            management.ProductReviewsDataTable(productReviewList);
+            //management.RetriveRecordsFromDataTable();
+            //UC-10
+            //management.AveragePerProductId(productReviewList);
+            //UC-11
+            //management.RetrieveRecordsUsingReview(productReviewList, "nice");
+            //UC-12
+            management.RetriveRecordsFromDataTableUsingUserID();
+        }
+    }
+}
